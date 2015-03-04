8f12cbe1d94773992c11d8dad938b17edfd1fb9a
Tom Canac <contact@tomcanac.com>
2015-03-03 23:34:46 -0500

    layout udpate

--- js/application.js
+++ js/application.js
@@ -1,15 +1,45 @@
 /** Initialisation of the application */
 var App = Ember.Application.create({
-	LOG_TRANSITIONS: true
+    LOG_TRANSITIONS: true
 });
 
 /** Routers */
 App.Router.map(function() {
-	this.route('tracks');
-	this.route('track', { path: '/tracks/:id' });
+    this.route('tracks');
+    this.route('track', { path: '/tracks/:id' });
 });
 
 /** Controllers */
 App.IndexController = Ember.Controller.extend({
     userName : "tmos"
 });
+
+/** Routes */
+App.TracksRoute = Ember.Route.extend({
+  model: function() {
+    return App.TRACKS;
+  }
+});
+
+App.TRACKS = [
+    {
+        id:1,
+        title:"Pages read",
+        creationDate:"26/10/1992",
+        type:1,
+        values:
+        [
+            12,
+            34,
+            67,
+            89,
+            12,
+            34,
+            98,
+            15,
+            62,
+            93,
+            45
+        ]
+    }
+];

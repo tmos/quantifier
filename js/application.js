78a2b1e642d59b47ba52625ca8fee547f839e4e3
Tom Canac <contact@tomcanac.com>
2015-03-14 16:14:59 -0400

    better views + improved code (more emberish)

--- js/application.js
+++ js/application.js
@@ -2,11 +2,13 @@
 var App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 
 /** Routers */
 App.Router.map(function() {
-    this.route('tracks');
-    this.route('track', { path: '/tracks/:id' });
+    this.resource('tracks');
+    this.resource('track', { path: 'tracks/:track_id' });
 });
 
 /** Controllers */
@@ -17,77 +19,92 @@ App.ApplicationController = Ember.Controller.extend({
 App.IndexController = Ember.Controller.extend({
     
 });
-App.TracksController = Ember.Controller.extend({
-
+App.TracksController = Ember.ArrayController.extend({
+    
 });
 
 
 /** Routes */
 App.TracksRoute = Ember.Route.extend({
     model: function() {
-        return App.TRACKS;
-    }
-});
-App.TrackRoute = Ember.Route.extend({
-    model: function(params) {
-        return App.TRACKS.findBy('id', params.id); 
+        return this.store.findAll('track');
     }
 });
 
-App.TRACKS = [
-    {
-        id:1,
-        title:"Pages read",
-        creationDate:"26/10/1992",
-        type:1,
-        values:
-        [
-            12,
-            34,
-            67,
-            89,
-            12,
-            34,
-            98,
-            15,
-            62,
-            93,
-            45
-        ]
-    }, {
-        id:2,
-        title:"km runs",
-        creationDate:"26/04/2014",
-        type:1,
-        values:
-        [
-            12.5,
-            24.3,
-            42,
-            8,
-            23
-        ]
-    }, {
-        id:3,
-        title:"Beverages",
-        creationDate:"26/10/1992",
-        type:2,
-        values:
+/** Models */
+App.Track = DS.Model.extend({
+    title: DS.attr('string'),
+    creationDate: DS.attr('string'),
+    type: DS.attr('number'),
+    values: DS.attr('array'),
+});
+    App.Track.FIXTURES = [
         {
-            "Biere": 28,
-            "Vin": 1,
-            "Eau": 234
+            id:1,
+            title:"Pages read",
+            creationDate:"26/10/1992",
+            type:1,
+            values:
+            [
+                12,
+                34,
+                67,
+                89,
+                12,
+                34,
+                98,
+                15,
+                62,
+                93,
+                45
+            ]
+        }, {
+            id:2,
+            title:"km runs",
+            creationDate:"26/04/2014",
+            type:1,
+            values:
+            [
+                12.5,
+                24.3,
+                42,
+                8,
+                23
+            ]
+        }, {
+            id:3,
+            title:"Beverages",
+            creationDate:"26/10/1992",
+            type:2,
+            values:
+            {
+                "Biere": 28,
+                "Vin": 1,
+                "Eau": 234
+            }
+        }, {
+            id:4,
+            title:"Beverages",
+            creationDate:"26/10/1992",
+            type:2,
+            values:
+            {
+                "Biere": 28,
+                "Vin": 1,
+                "Eau": 234
+            }
         }
-    }, {
-        id:4,
-        title:"Beverages",
-        creationDate:"26/10/1992",
-        type:2,
-        values:
+    ];
+
+App.Proportion = DS.Model.extend({
+    title: DS.attr('string'),
+    creationDate: DS.attr('string'),
+    tracks: DS.hasMany('track'),
+});
+    App.Proportion.FIXTURES = [
         {
-            "Biere": 28,
-            "Vin": 1,
-            "Eau": 234
+            title:"test proportion",
+            creationDate:"26/10/1992",
+            tracks: [1,2]
         }
-    }
-];
+    ];

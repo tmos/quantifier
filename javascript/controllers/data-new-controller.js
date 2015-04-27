3047e3fab5b10320034c04736bb6d4a7a0e9de3a
Tom Canac <contact@tomcanac.com>
2015-04-27 11:47:55 -0400

    correction post + amelioration interface

--- javascript/controllers/data-new-controller.js
+++ javascript/controllers/data-new-controller.js
@@ -1,5 +1,7 @@
 angular.module('quantifier')
     .controller('DataNewController', function () {
+        controller = this;
+        controller .dateChosen = new Date();
         this.changeDate = function () {
             swal({
                 title: "Date",
@@ -7,22 +9,40 @@ angular.module('quantifier')
                 type: "input",
                 inputType:"date",
                 showCancelButton: true,
-                closeOnConfirm: false,
                 animation: "slide-from-top",
-                inputPlaceholder: new Date().toISOString()
+                inputPlaceholder: controller.dateChosen.toISOString()
             }, function (inputValue) {
                 if (inputValue === false) return false;
                 if (inputValue === "") {
                     swal.showInputError("You need to write something!");
                     return false
                 }
-                swal("Nice!", "You wrote: " + inputValue, "success");
+                controller.dateChosen = inputValue;
             });
         };
+
+        this.comment = function () {
+            swal({
+                title: "Comment",
+                text: "Add something about this record",
+                type: "input",
+                showCancelButton: true,
+                animation: "slide-from-top",
+                inputPlaceholder: "…"
+            }, function (inputValue) {
+                if (inputValue === false) return false;
+                if (inputValue === "") {
+                    swal.showInputError("You need to write something!");
+                    return false
+                }
+                controller.comment = inputValue;
+            });
+        };
+
         this.create = function (track) {
             $http({method: 'POST', url: 'api/web/api/data', data: controller.data})
-                .success(function () {
-
+                .success(function (data) {
+                    console.log(data);
                 })
                 .error(function () {
 
--- javascript/controllers/track-show-controller.js
+++ javascript/controllers/track-show-controller.js
@@ -6,6 +6,6 @@ angular.module('quantifier')
                 controller.track = data;
             })
             .error(function(){
-                controller.track = JSON.parse('{"id":16,"name":"Whisky drunk","creator":"Spieldy","date":"2015-04-25T17:20:48-0400","type":0,"evolutions":[1,2,3,4,5],"listings":[],"binaries":[]}');
+                controller.track = JSON.parse('{"id":18,"name":"KMs run","creator":"Spieldy","date":"2015-04-24T14:50:30-0400","type":0,"evolutions":[{"id":13,"value":5.5,"date_creation":"2015-04-26T15:05:53-0400","date_chosen":"2015-04-26T13:26:34-0400","comment":"Sunny"},{"id":14,"value":2.12,"date_creation":"2015-04-26T15:06:08-0400","date_chosen":"2015-04-24T10:50:30-0400","comment":"Under rain"}],"listings":[],"binaries":[]}');
             });
     });
